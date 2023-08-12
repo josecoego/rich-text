779553a3dc383a4f56893b89f14d4133ab146ed4
adm-JCOEGOMIGUE <jose.coegomiguelez@zurich.com>
2023-08-12 12:04:15 +0200

    add styles-scripts

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,39 @@
-import React from "react";
+import React, { useEffect } from "react";
 import HtmlToReact from "html-to-react";
 
 const HtmlToReactParser: any = HtmlToReact.Parser; /**TODO: Change any */
 const htmlToReactParser = new HtmlToReactParser();
 
 function App() {
-  const htmlInput = "<div><h1>Title</h1><p>A paragraph</p></div>";
+  const htmlInput =
+    "<div id='test1' class='bg-red'><h1>Title</h1><p>A paragraph</p></div>";
+  const scriptContent = `console.log('hi')
+    var button1 = document.getElementById('test1')
+    button1.addEventListener('click', function(){
+       alert('click')
+    })
+  `;
+
   const reactElement = htmlToReactParser.parse(htmlInput);
-  return <div className="App">{reactElement}</div>;
+  useEffect(() => {
+    const script = document.createElement("script");
+    script.innerHTML = scriptContent;
+    document.body.appendChild(script);
+
+    return () => {
+      document.body.removeChild(script);
+    };
+  }, []);
+
+  return (
+    <div className="App">
+      <style>{`.bg-red{
+      background-color: red;
+    }`}</style>
+      {reactElement}
+      <button id="test2">Test2</button>
+    </div>
+  );
 }
 
 export default App;
